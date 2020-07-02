f0656a2b3000535a93c4be684a435612691cda99
liyipeng <liyipeng@changingedu.com>
2020-07-02 11:18:20 +0800

    init

---
+++ src/components/Demo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        my-demo
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
